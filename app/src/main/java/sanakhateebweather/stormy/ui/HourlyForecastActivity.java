817b252a06cbaef069f94aeef8c0758139275fe6
Sana <sanakhateeb14@hotmail.com>
2017-05-30 22:32:38 -0500

    Both hourly and 7-day lists working -- RecyclerView & ListView

---
+++ app/src/main/java/sanakhateebweather/stormy/ui/HourlyForecastActivity.java
@@ -0,0 +1,50 @@
+package sanakhateebweather.stormy.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import sanakhateebweather.stormy.R;
+import sanakhateebweather.stormy.R2;
+import sanakhateebweather.stormy.adapters.HourAdapter;
+import sanakhateebweather.stormy.weather.Hour;
+
+public class HourlyForecastActivity extends AppCompatActivity {
+
+    private Hour[] mHours;
+
+    @BindView(R2.id.recyclerView) RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        HourAdapter adapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        //for fixed size views
+        mRecyclerView.setHasFixedSize(true);
+    }
+
+}
--- app/src/main/java/sanakhateebweather/stormy/weather/Hour.java
+++ app/src/main/java/sanakhateebweather/stormy/weather/Hour.java
@@ -1,11 +1,19 @@
 package sanakhateebweather.stormy.weather;
 
-public class Hour {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Hour implements Parcelable{
     private long mTime;
     private String mSummary;
     private double mTemperature;
     private String mIcon;
     private String mTimezone;
+
+    public Hour() {};
 
     public long getTime() {
         return mTime;
@@ -23,8 +31,8 @@ public class Hour {
         mSummary = summary;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public int getTemperature() {
+        return (int)Math.round(mTemperature);
     }
 
     public void setTemperature(double temperature) {
@@ -33,6 +41,11 @@ public class Hour {
 
     public String getIcon() {
         return mIcon;
+    }
+
+    public int getIconId()
+    {
+        return Forecast.getIconId(mIcon);
     }
 
     public void setIcon(String icon) {
@@ -46,4 +59,46 @@ public class Hour {
     public void setTimezone(String timezone) {
         mTimezone = timezone;
     }
+
+    public String getHour() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h a");
+        Date date = new Date(mTime * 1000);
+        return formatter.format(date);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeString(mSummary);
+        dest.writeDouble(mTemperature);
+        dest.writeString(mIcon);
+        dest.writeString(mTimezone);
+    }
+
+    private Hour(Parcel in)
+    {
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperature = in.readDouble();
+        mIcon = in.readString();
+        mTimezone = in.readString();
+    }
+
+    public static final Creator<Hour> CREATOR = new Creator<Hour>() {
+        @Override
+        public Hour createFromParcel(Parcel source) {
+            Hour hour = new Hour(source);
+            return hour;
+        }
+
+        @Override
+        public Hour[] newArray(int size) {
+            return new Hour[size];
+        }
+    };
 }
