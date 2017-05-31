c6adc2d9c941c43192efa3778dc5d45d29095205
Sana <sanakhateeb14@hotmail.com>
2017-05-31 00:13:08 -0500

    OnItemClick for both views

--- app/src/main/java/sanakhateebweather/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/sanakhateebweather/stormy/ui/DailyForecastActivity.java
@@ -1,25 +1,37 @@
 package sanakhateebweather.stormy.ui;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import sanakhateebweather.stormy.R;
 import sanakhateebweather.stormy.adapters.DayAdapter;
 import sanakhateebweather.stormy.weather.Day;
 
-public class DailyForecastActivity extends ListActivity {
+public class DailyForecastActivity extends Activity {
+
+    //changed to regular activity, can extend ListActivity for built in stuff (whichever works)
     private Day[] mDays;
+    @BindView(android.R.id.list) ListView mListView;
+    @BindView(android.R.id.empty) TextView mEmptyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
@@ -27,8 +39,33 @@ public class DailyForecastActivity extends ListActivity {
         mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
 
         DayAdapter adapter = new DayAdapter(this, mDays);
-        setListAdapter(adapter);
+        mListView.setAdapter(adapter);
+        //setListAdapter(adapter)
+        mListView.setEmptyView(mEmptyTextView);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+                String conditions = mDays[position].getSummary();
+                String highTemp = mDays[position].getTemperatureMax() + "";
+                String message = String.format("On %s the high will be %s and it will be %s ", dayOfTheWeek, highTemp, conditions);
+                Toast.makeText(DailyForecastActivity.this, message,Toast.LENGTH_LONG).show();
+
+            }
+        });
 
     }
+/*  FOR EXTEND LISTACTIVITY
 
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String dayOfTheWeek = mDays[position].getDayOfTheWeek();
+        String conditions = mDays[position].getSummary();
+        String highTemp = mDays[position].getTemperatureMax() + "";
+        String message = String.format("On %s the high will be %s and it will be %s ", dayOfTheWeek, highTemp, conditions);
+        Toast.makeText(this, message,Toast.LENGTH_LONG);
+
+    }*/
 }
--- app/src/main/java/sanakhateebweather/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/sanakhateebweather/stormy/ui/HourlyForecastActivity.java
@@ -3,13 +3,9 @@ package sanakhateebweather.stormy.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import java.util.Arrays;
 
@@ -37,7 +33,7 @@ public class HourlyForecastActivity extends AppCompatActivity {
 
         mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
 
-        HourAdapter adapter = new HourAdapter(mHours);
+        HourAdapter adapter = new HourAdapter(this, mHours);
         mRecyclerView.setAdapter(adapter);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
